097b6f4866b955cd8d82e6bd317f93621ceb45ec
Prince-Affedzie <paffedzie001@st.ug.edu.gh>
2025-05-08 17:18:26 +0000

    Phase 3

--- Backend/app.js
+++ Backend/app.js
@@ -15,7 +15,7 @@ app.use(cookieParser())
 app.use(express.json())
 
 app.use(cors({
-    origin:"https://apartment-rental-management.vercel.app/",
+    origin:"https://apartment-rental-management.vercel.app",
     credentials:true
 }))
 app.use('/uploads',express.static('uploads'))
