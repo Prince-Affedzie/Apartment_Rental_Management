affdacadc2496528dc003d6e95152f293c6fe194
Prince-Affedzie <paffedzie001@st.ug.edu.gh>
2025-10-11 11:51:46 +0000

    added the back the password authentication

--- Backend/Controllers/UserController.js
+++ Backend/Controllers/UserController.js
@@ -55,10 +55,10 @@ const login = async (req, res) => {
     if (!user) {
       return res.status(404).json({ message: "User Account not found" });
     }
-    /*const isPasswordMatch = await bcrypt.compare(password, user.password);
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
     if (!isPasswordMatch) {
       return res.status(400).json({ message: "Invalid Username or Password" });
-    }*/
+    }
 
     const token = jwt.sign(
       { id: user._id, role: user.role },
